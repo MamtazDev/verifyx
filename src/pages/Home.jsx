f166e8ebb7e9127c7f5ff3bca121652523da60c7
nipaaa <akternipa941@gmail.com>
2024-02-16 14:46:12 +0600

    home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,17 +12,16 @@ const Home = () => {
         {/* brand nav -/ end */}
 
         {/* singin/singup */}
-        <h4 className="title-style">
-          Get Started
-        </h4>
+        <h4 className="title-style">Get Started</h4>
         {/* singin/singup -/end */}
-        <div className="btn_wrapper flex flex-col gap-4 items-center">
+        <div className="btn_wrapper flex flex-col gap-[38px] lg:gap-4 items-center">
           <Link
             to={"/create-account"}
-            className="inline-block w-[288px] py-[18px] shadow-shadowOne text-center text-lg font-medium rounded-[25px] bg-[#4171FC] text-white">
+            className="inline-block w-full lg:w-[288px] py-[21px] lg:py-[18px] shadow-shadowOne text-center text-lg font-medium rounded-[25px] bg-[#4171FC] text-white"
+          >
             Sign up
           </Link>
-          <Link className="inline-block w-[288px] py-[18px] shadow-shadowOne text-center text-lg font-medium rounded-[25px] border border-[#4171FC] text-[#4171FC]">
+          <Link className="inline-block w-full lg:w-[288px] py-[21px] lg:py-[18px] shadow-shadowOne text-center text-lg font-medium rounded-[25px] border border-[#4171FC] text-[#4171FC]">
             Login
           </Link>
         </div>
